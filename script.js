df7a817de2536f3d84b737123a7283fba73ce254
Flávio Novaes de Oliveira <flavio.novaes29@gmail.com>
2026-06-18 23:04:34 -0300

    feat: botao topo quando scrolla a pag

--- script.js
+++ script.js
@@ -282,3 +282,21 @@ function atualizarMetas() {
 
 }
 
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+        btnTopo.classList.add("show");
+    } else {
+        btnTopo.classList.remove("show");
+    }
+
+});
+
+btnTopo.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
